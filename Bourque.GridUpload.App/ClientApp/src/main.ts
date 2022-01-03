f1a70a195687bfbc17fa4714750b0923d76c6740
Charith Prabhagya <charith.prabhagya@xeynergy.com>
2022-01-03 19:06:38 +0530

    Fixed url

--- Bourque.GridUpload.App/ClientApp/src/main.ts
+++ Bourque.GridUpload.App/ClientApp/src/main.ts
@@ -4,17 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
+export function getBaseUrl(): string {
+    return environment.shipperBiOptions.shipperBiApiUri;
 }
 
-const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
-];
+const providers = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic(providers)
+    .bootstrapModule(AppModule)
+    .catch((err) => console.log(err));
