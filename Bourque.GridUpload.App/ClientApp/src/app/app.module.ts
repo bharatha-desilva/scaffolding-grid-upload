c1f184077c0c9b934b3a15cd90df59110bd7d780
Bharatha De Silva <bharathanet@gmail.com>
2022-01-05 11:19:13 +0530

    [DEV] Added DevExtreme demo stuffs

--- Bourque.GridUpload.App/ClientApp/src/app/app.module.ts
+++ Bourque.GridUpload.App/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {TemplateDataEditComponent} from "./data/template-data-edit.component";
+import {DevExtremeModule, DxButtonModule, DxDataGridModule} from "devextreme-angular";
+import {DevExtremeDemoComponent} from "./grid-demo/dev-extreme-demo.component";
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    TemplateDataEditComponent,
+    DevExtremeDemoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +31,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: 'dev-extreme', component: DevExtremeDemoComponent },
+    ]),
+    DxDataGridModule,
+    DxButtonModule,
+    DevExtremeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Bourque.GridUpload.App/ClientApp/src/app/data/template-definition.ts
@@ -0,0 +1,48 @@
+export class TemplateDef {
+
+  keyField!: string;
+  columns!: TemplateColumnDef[];
+}
+
+export class TemplateColumnDef {
+  property!: string;
+  caption?: string;
+  dataType?: DataType;
+  dataLookup?: TemplateColumnDataLookUp;
+  validationRules?: TemplateDataValidationRule[]
+}
+
+export class TemplateColumnDataLookUp {
+  property!: string;
+  caption!: string;
+  data!: any[]
+}
+
+export class TemplateDataValidationRule {
+  type!: ValidationType;
+  message!: string;
+  pattern!: string;
+  min!: Date | number;
+  max!: Date | number;
+  maxlength!: string | number | null
+  validate!: Function;
+  compare!: Function;
+}
+
+export enum DataType {
+  NUMBER="number",
+  DATE="date",
+  STRING="string",
+}
+
+export enum ValidationType {
+  ASYNC="async",
+  REQUIRED="required",
+  NUMERIC="numeric",
+  RANGE="range",
+  STRING_LENGTH="stringLength",
+  CUSTOM="custom",
+  COMPARE="compare",
+  EMAIL="email",
+  PATTERN="pattern"
+}
